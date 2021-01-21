c81b0aca7331a3059dfa8f56d64e9838a0ba0723
Daniel Clarke <daniel@danielrlc.com>
2021-01-21 04:26:32 +0200

    removed semicolons from js, in line with styling in the Stimulus docs

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-(() => {
-  const application = Stimulus.Application.start();
+;(() => {
+  const application = Stimulus.Application.start()
 
   application.register(
     'sentence',
     class extends Stimulus.Controller {
       static get targets() {
-        return ['text'];
+        return ['text']
       }
       initialize() {
-        this.buildWordsData(false);
-        this.textTarget.innerHTML = this.text;
+        this.buildWordsData(false)
+        this.textTarget.innerHTML = this.text
       }
 
       rawText =
-        'When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.';
-      wordsData = [];
+        'When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.'
+      wordsData = []
 
       buildWordsData(wordsAreShown) {
-        this.wordsData = [];
+        this.wordsData = []
         this.rawText.split(' ').map((word, position) => {
           this.wordsData = [
             ...this.wordsData,
             { word, position, isShown: wordsAreShown },
-          ];
-        });
-        this.buildText();
+          ]
+        })
+        this.buildText()
       }
-      allWordsAreShown = false;
+      allWordsAreShown = false
 
-      text = '';
+      text = ''
       buildText() {
-        this.text = '';
+        this.text = ''
         this.wordsData.map(({ word, position, isShown }) => {
           this.text += `<span id="${position}" data-action="click->sentence#flipWord">${
             isShown ? word : '____'
-          }</span> `;
-        });
-        this.textTarget.innerHTML = this.text;
+          }</span> `
+        })
+        this.textTarget.innerHTML = this.text
       }
 
       flipSentence() {
         if (this.allWordsAreShown) {
-          this.buildWordsData(false);
-          this.allWordsAreShown = false;
+          this.buildWordsData(false)
+          this.allWordsAreShown = false
         } else {
-          this.buildWordsData(true);
-          this.allWordsAreShown = true;
+          this.buildWordsData(true)
+          this.allWordsAreShown = true
         }
       }
 
       flipWord(event) {
-        console.log(event.target.id);
+        wordPosition = event.target.id
       }
     },
-  );
-})();
+  )
+})()
